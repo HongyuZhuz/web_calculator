094af5db540c585f83482d728f150823e3f6f13a
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-10-25 14:30:49 +1100

    the calculator can be used, still need to work on edge stage

--- calculate.js
+++ calculate.js
@@ -2,16 +2,43 @@ const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
 const display = document.querySelector('.calculator__display');
 
+const calculate = (n1,operator, n2) =>{
+    let result = '';
+
+    if(operator === 'add'){
+        result = parseFloat(n1)+parseFloat(n2);
+    }else if (operator === 'subtract'){
+        result = parseFloat(n1)-parseFloat(n2);
+    }else if (operator === 'multiply'){
+        result = parseFloat(n1)*parseFloat(n2);
+    }else if (operator === 'divide'){
+        result = parseFloat(n1)/parseFloat(n2);
+    }
+
+    return result
+}
+
+
 keys.addEventListener('click', e=>{
+    
     if(e.target.matches('button')){
         const key = e.target;
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
 
+        const previousKeyType = calculator.dataset.previousKeyType;
+
+        //// Remove is-depressed class from all keys
+        Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'))
+
+        
+
         if(!action){ //if press the number button
-            if(displayedNum ==='0'){
-                display.textContent = keyContent;
+            if(displayedNum ==='0' || previousKeyType === 'operator'){
+                display.textContent = keyContent; //reset the display number
+                calculator.dataset.previousKeyType = ''; // Clear the previousKeyType
             }
             else{
                 display.textContent = displayedNum + keyContent;
@@ -21,10 +48,15 @@ keys.addEventListener('click', e=>{
         if(
             action ==='add'||
             action === 'subtract' ||
-            action === 'multply' ||
+            action === 'multiply' ||
             action === 'divide'
             ){
-            console.log('operator key!')
+                key.classList.add('is-depressed');
+
+                calculator.dataset.previousKeyType = 'operator';
+                calculator.dataset.firstValue = displayedNum;//save the first value
+                calculator.dataset.operator = action; // save the operation key
+                console.log(calculator.dataset);
             };
         if (action === 'decimal'){
             display.textContent = displayedNum + '.';
@@ -34,8 +66,13 @@ keys.addEventListener('click', e=>{
             console.log('clear key!')
         };
 
-        if(action ==='calculate'){console.log('equal key!')}
+        if(action ==='calculate'){
+            const firstValue = calculator.dataset.firstValue;
+            const secondValue = displayedNum;
+            const operator = calculator.dataset.operator;
 
+            display.textContent = calculate(firstValue,operator, secondValue);
+        }
     };
 });
 
