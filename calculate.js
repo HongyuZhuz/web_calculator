9d6548ec0ad32ca40f16d94ad260ad8cac665298
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-11-09 14:36:20 +1100

    considered the edge case of decimal

--- calculate.js
+++ calculate.js
@@ -1,18 +1,18 @@
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
-const display = document.querySelector('.calculator__display');
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
 
 const calculate = (n1,operator, n2) =>{
-    let result = '';
+    let result = ''
 
     if(operator === 'add'){
-        result = parseFloat(n1)+parseFloat(n2);
+        result = parseFloat(n1)+parseFloat(n2)
     }else if (operator === 'subtract'){
-        result = parseFloat(n1)-parseFloat(n2);
+        result = parseFloat(n1)-parseFloat(n2)
     }else if (operator === 'multiply'){
-        result = parseFloat(n1)*parseFloat(n2);
+        result = parseFloat(n1)*parseFloat(n2)
     }else if (operator === 'divide'){
-        result = parseFloat(n1)/parseFloat(n2);
+        result = parseFloat(n1)/parseFloat(n2)
     }
 
     return result
@@ -23,11 +23,11 @@ keys.addEventListener('click', e=>{
     
     if(e.target.matches('button')){
         const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
 
-        const previousKeyType = calculator.dataset.previousKeyType;
+        const previousKeyType = calculator.dataset.previousKeyType
 
         //// Remove is-depressed class from all keys
         Array.from(key.parentNode.children)
@@ -43,9 +43,10 @@ keys.addEventListener('click', e=>{
             else{
                 display.textContent = displayedNum + keyContent;
             }
+            calculator.dataset.previousKeyType = 'number'
         };
 
-        if(
+        if(//if press the operator button
             action ==='add'||
             action === 'subtract' ||
             action === 'multiply' ||
@@ -53,20 +54,30 @@ keys.addEventListener('click', e=>{
             ){
                 key.classList.add('is-depressed');
 
-                calculator.dataset.previousKeyType = 'operator';
+                calculator.dataset.previousKeyType = 'operator'
                 calculator.dataset.firstValue = displayedNum;//save the first value
                 calculator.dataset.operator = action; // save the operation key
                 console.log(calculator.dataset);
             };
-        if (action === 'decimal'){
-            display.textContent = displayedNum + '.';
+        if (action === 'decimal'){//if press the decimal button
+            
+            if (previousKeyType === 'operator'){
+                display.textContent = '0.'
+            }else if(!displayedNum.includes('.')){
+                display.textContent = displayedNum + '.'
+            }
+            calculator.dataset.previousKeyType = 'decimal'
+            
         };
         
-        if(action === 'clear'){
+        if(action === 'clear'){//if press the AC button
+            calculator.dataset.previousKeyType = 'clear'
             console.log('clear key!')
         };
 
-        if(action ==='calculate'){
+        if(action ==='calculate'){//if press the calculate button
+
+            calculator.dataset.previousKeyType = 'calculate'
             const firstValue = calculator.dataset.firstValue;
             const secondValue = displayedNum;
             const operator = calculator.dataset.operator;
